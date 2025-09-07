d869d298ac1bd123f611f12daf1d6c51e67aad99
Mario Villoria <mariovilloria@gmail.com>
2025-09-06 21:48:43 -0500

    Mueve drop_database dentro del fixture y solo para MongoDB real

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,15 @@
 import pytest
-from app import app as flask_app, db
-from bson import ObjectId
+from app import app as flask_app
+from app import db
 
 @pytest.fixture
 def app():
     flask_app.config.update({
         "TESTING": True,
-        "MONGO_URI": "mongomock://localhost/vehiculos_test",  # base en memoria     
+        "MONGO_URI": "mongomock://localhost/vehiculos_test",
         "WTF_CSRF_ENABLED": False
     })
     yield flask_app
-# Limpieza solo si NO es mongomock
-if not flask_app.config["MONGO_URI"].startswith("mongomock"):
-    db.client.drop_database("vehiculos_test")
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
+    # Limpieza solo si NO es mongomock
+    if not flask_app.config["MONGO_URI"].startswith("mongomock"):
+        db.client.drop_database("vehiculos_test")
