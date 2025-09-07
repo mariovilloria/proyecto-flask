dafaf591572ec6a7a9a39ce51acc5a767c6c69e5
Mario Villoria <mariovilloria@gmail.com>
2025-09-07 14:51:10 -0500

    Usa mongomock sin tocar cliente real

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,15 @@
 import pytest
-import mongomock
 from app import create_app
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def app():
-    """
-    Crea una app Flask para testing con una DB simulada (mongomock).
-    """
-    # Inicializa la app en modo testing
     app = create_app("testing")
-    app.config.update({
-        "TESTING": True,
-        "WTF_CSRF_ENABLED": False  # Desactiva CSRF para que los tests de login funcionen
-    })
-
-    # Usar mongomock en lugar de Mongo real
-    mock_client = mongomock.MongoClient()
-    app.db = mock_client["test_db"]
-
     yield app
 
 @pytest.fixture
 def client(app):
-    """
-    Cliente de pruebas que simula requests HTTP.
-    """
     return app.test_client()
 
 @pytest.fixture
 def db(app):
-    """
-    Acceso directo a la base de datos mockeada (para poblarla en tests).
-    """
     return app.db
